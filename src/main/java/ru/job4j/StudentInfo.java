714d586c2d4e76ecd4cf96668c05059db3ed40e8
Артём <iartem1405@gmail.com>
2023-02-15 00:10:08 +0300

    4.1. Разбор первой программы. [#159033 #391349]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
       System.out.println("Artem Lukichev");
       System.out.println("14.05.1997");
     }
