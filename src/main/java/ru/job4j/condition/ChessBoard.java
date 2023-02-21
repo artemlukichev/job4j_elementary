674d1b9b2010465983be37f467e5d1a702b80d81
Артём <iartem1405@gmail.com>
2023-02-22 00:40:20 +0300

    4.3.6. Math.abs [#373616 #394039]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -5,10 +5,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 == x2 || y1 == y2) {
-                rsl = Math.abs(x2 - x1);
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-            } else if (isBishop(x1, y1, x2, y2)) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = (int) Math.sqrt((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) / 2);
             }
         }
@@ -17,9 +14,5 @@ public class ChessBoard {
 
     private static boolean isValid(int coordinate) {
         return coordinate >= 0 && coordinate <= 7;
-    }
-
-    private static boolean isBishop(int x1, int y1, int x2, int y2) {
-        return Math.abs(x2 - x1) == Math.abs(y2 - y1);
     }
 }
