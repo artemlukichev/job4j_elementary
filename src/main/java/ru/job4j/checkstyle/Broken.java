9895f0299af07e18c6652286c2a2de2590c7bdd5
Артём <iartem1405@gmail.com>
2023-03-13 23:38:38 +0300

    Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,23 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String NEW_VALUE = "";
+    private static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surName;
-    String name;
+    private String surName;
+    private String name;
 
-    void echo() {
+    public Broken() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
-    }
-
-    void broken() {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
